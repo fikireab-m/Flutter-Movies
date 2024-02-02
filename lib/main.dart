4a2a34e1f062de79da0de35c726519c68995f082
fikireab-m <fikireab.meku@gmail.com>
2024-02-02 21:16:59 +0300

    trying materials dynamic_color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,16 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlMovies',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+    return DynamicColorBuilder(
+      builder: (lightColorScheme, darkColorScheme) {
+        return MaterialApp(
+          theme: ThemeData(
+            colorScheme: darkColorScheme,
+          ),
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
